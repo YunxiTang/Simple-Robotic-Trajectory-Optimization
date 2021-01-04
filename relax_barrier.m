7f04b1e844a6cd51d5666faf3a8c59bccf18f36e
YunxiTang <54171095+YunxiTang@users.noreply.github.com>
2021-01-04 15:55:23 +0800

    Add files via upload

---
+++ relax_barrier.m
@@ -0,0 +1,36 @@
+%%% relaxed barrier function
+clc;
+clear;
+x1 = -15:0.1:200;
+x2 = 0:0.00001:200;
+x3 = 0:0.1:200;
+N = length(x1);
+y1 = zeros(N,1);
+for i=1:N
+    z = x1(i);
+    y1(i) = relaxed_barrier(z, 5);
+end
+
+y2 = barrier(x2);
+y3 = 1 ./ x3;
+figure(1);hold on;
+plot(x1,y1,'b','LineWidth',3.0);hold on;
+plot(x2,y2,'r-.','LineWidth',2.0); hold on;
+plot(x3,y3,'k','LineWidth',2.0);
+xlabel('x','Interpreter','latex','FontSize',14);
+legend('Relaxed Log Barrier Function','Log Barrier Function','$1/x$ Barrier Function',...
+       'Interpreter','latex','Fontsize',14);
+grid on;
+
+
+function value = relaxed_barrier(x, delta)
+    if delta < x
+        value = -log(x);
+    else
+        value = 1/2*(((x-2*delta)/delta).^2-1)-log(delta);
+    end        
+end
+
+function value = barrier(x)
+        value = -log(x);      
+end
