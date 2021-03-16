3b917695a2b8b5355194ccac2a9738cda0e2ac37
YunxiTang <1155136594@link.cuhk.edu.hk>
2021-03-16 16:53:38 +0800

    add constrained single shooting method

--- 2D_Car_constrained/@constraint/constraint.m
+++ 2D_Car_constrained/@constraint/constraint.m
@@ -4,11 +4,9 @@ classdef constraint < handle
         % cineq(x, u) <= 0
     
     properties
-        n_ineq = 0,         % number of inequality constraint
-        lambda = 0.0,       % Lagrangian Multiplier for inequality constraints
-        rho = 10,           % Multiplier for Augmented Penalty
-        phi = 10            % scaling parameter for rho, rho <- phi * rho
-        x_obstacle = []     % [nx+1, n_ineq], store obstacles
+        n_ineq = 0.0,       % Number of inequality constraint
+        x_obstacle = [],    % [nx+1, n_ineq], store obstacles
+        active_set = []     % check the active constraints
     end
     
     methods
@@ -20,29 +18,39 @@ classdef constraint < handle
             obj.x_obstacle = obstacles;
         end
         
-        function outputArg = active_set(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function Imu = active_check(obj, x, u, lambda, mu)
+            %METHOD1 check the active set
+            Imu = zeros(obj.n_ineq, obj.n_ineq);
+            h = obj.c_ineq(x, u);
+            for i=1:obj.n_ineq
+                if h(i) > 0.0 || lambda(i) > 0.0
+                    Imu(i,i) = mu(i);
+                end
+            end
         end
         
-        function g = c_eq(obj, x, u)
-            % eqaulity constraint
-            g = zeros(obj.n_eq, 1);
-            
+        function h = c_ineq(obj, x, u)
+            % ineqaulity constraint (make sure it is less/equal (h<=0) than 0)
+            H = [1 0 0 0;
+                 0 1 0 0];
+            for k=1:obj.n_ineq
+                r = obj.x_obstacle(3,k);
+                x_obs = obj.x_obstacle(1:2,k);
+                h(k) = r*r - (H*x - x_obs)' * (H*x - x_obs);
+            end
+            h = reshape(h,[obj.n_ineq,1]);
         end
         
-        function h = c_ineq(obj, x, u, x_obs1, r1, x_obs2, r2)
-            % ineqaulity constraint (make sure it is less/equal than 0)
-            h = zeros(obj.n_ineq, 1);
-            H = diag([1.0 1.0 0.0 0.0]);
-            h(1) = r1*r1 - (H*x - x_obs1)' * (H*x - x_obs1);
-            h(2) = r2*r2 - (H*x - x_obs2)' * (H*x - x_obs2);
+        function [AL_J] = AL_Term(obj, x, u, lambda, Imu)
+            AL_J = (lambda + 1/2 * Imu * obj.c_ineq(x,u))' * obj.c_ineq(x,u);
         end
+      
         
         function Flag = plot_obstacle(obj, map)
-            figure(map);
-            aplha=0:pi/40:2*pi;
+            if nargin > 1
+                figure(map);
+            end
+            aplha=0:0.01:2*pi;
             for i=1:obj.n_ineq
                 % the i-th circle
                 r = obj.x_obstacle(3,i); 
@@ -51,15 +59,21 @@ classdef constraint < handle
                 cx = xi_c + r*cos(aplha);
                 cy = yi_c + r*sin(aplha);
                 clr = abs([sin(i/1.2) sin(i/1.5) sin(i)]);
-                plot(cx, cy, '-'); hold on;
+                plot(cx, cy, '-','Color',clr); hold on;
                 fill(cx, cy, clr);
                 axis equal;
             end
             xlabel('$x$','Interpreter','latex','FontSize',15);
             ylabel('$y$','Interpreter','latex','FontSize',15);
+            title("$2D\;Map$", 'Interpreter','latex','FontSize',15);
             grid on;
             Flag = 1;
         end
+    end
+    methods (Static)
+       %%% After code generation to class, Add the function declaration
+       %%% here !!!
+       [cx,cu] = algrad(x,u);
     end
 end
 
